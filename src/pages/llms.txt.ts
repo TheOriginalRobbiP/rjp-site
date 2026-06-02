70f7b01ba7a4b3869185324959fa0e3b1f7765c0
Robin Pretorius <rjpretorius89@gmail.com>
2026-06-02 09:02:21 +0100

    feat: serve robots.txt and llms.txt as dynamic server routes

---
+++ src/pages/llms.txt.ts
@@ -0,0 +1,11 @@
+import llmsContent from '../../public/llms.txt?raw';
+
+export const prerender = false;
+
+export async function GET() {
+  return new Response(llmsContent, {
+    headers: {
+      'content-type': 'text/plain; charset=utf-8',
+    },
+  });
+}
